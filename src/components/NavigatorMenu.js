4d72a6efaf304467067c02d282c36a5d0909b86c
Alex Lecco <alexlecco24@gmail.com>
2024-03-14 19:23:30 -0300

    fix navigation items layout and add shadow to them

--- src/components/NavigatorMenu.js
+++ src/components/NavigatorMenu.js
@@ -15,7 +15,7 @@ function NavigatorMenu({ theme, scroller, t }) {
   return (
     <>
       {width < MIN_WIDTH_SIZE ? (
-        <ul className={`${theme}-transparent md:flex md:flex-row md:justify-center md:align-middle md:pb-8`} >
+        <ul className={`${theme}-transparent md:flex md:flex-row md:justify-center md:align-middle`} >
           <li className='md:pr-28 py-2 my-2 md:pb-0 cursor-pointer'>
             <p
               className='mb-2 background-shadow'
@@ -25,29 +25,41 @@ function NavigatorMenu({ theme, scroller, t }) {
             </p>
           </li>
           <li className='md:pr-28 py-2 my-2 md:pb-0 cursor-pointer'>
-            <p onClick={() => scrollToElement("skillsElement")}>
+            <p 
+              className='mb-2 background-shadow'
+              onClick={() => scrollToElement("skillsElement")}
+            >
               {t("presentation.item2")}
             </p>
           </li>
           <li className='md:pr-28 py-2 my-2 md:pb-0 cursor-pointer'>
-            <p onClick={() => scrollToElement("experienceElement")}>
+            <p
+              className='mb-2 background-shadow'
+              onClick={() => scrollToElement("experienceElement")}
+            >
               {t("presentation.item3")}
             </p>
           </li>
           <li className='md:pr-28 py-2 my-2 md:pb-0 cursor-pointer'>
-            <p onClick={() => scrollToElement("portfolioElement")}>
+            <p
+              className='mb-2 background-shadow'
+              onClick={() => scrollToElement("portfolioElement")}
+            >
               {t("presentation.item4")}
             </p>
           </li>
           <li className='py-2 my-2 md:pb-0 cursor-pointer'>
-            <p onClick={() => scrollToElement("contactElement")}>
+            <p
+              className='mb-2 background-shadow'
+              onClick={() => scrollToElement("contactElement")}
+            >
               {t("presentation.item5")}
             </p>
           </li>
         </ul>
       ) : (
         <ul
-          className={`${theme}-transparent md:flex md:flex-row md:justify-center md:align-middle md:pb-8`}
+          className={`${theme}-transparent md:flex md:flex-row md:justify-center md:align-middle md:pb-28`}
         >
           <li className='md:pr-28 pb-10 md:pb-0 cursor-pointer'>
             <p className='' onClick={() => scrollToElement("goalsElement")}>
